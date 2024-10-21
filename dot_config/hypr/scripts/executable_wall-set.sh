e1b86f07f55a9b075c46b7eea9542a5d4c4b712c
virt-10 <121334908+virt-10@users.noreply.github.com>
2024-10-21 08:52:56 -0500

    feat: update wall-set

--- dot_config/hypr/scripts/executable_wall-set.sh
+++ dot_config/hypr/scripts/executable_wall-set.sh
@@ -1,33 +1,64 @@
 #!/usr/bin/bash
 
-# wallpaper selector
 
-# gum style options
-gum style \
-    --foreground 212 --border-foreground 212 --border double \
-    --align center --width 100 --margin "1 2" --padding "2 4" --bold
-
-# wallpaper selector
-IMAGE_DIR="${HOME}/Pictures/Wallpapers"
-DEST_DIR="${HOME}/.cache/wallpaper"
+DEST_DIR="${HOME}/.cache/blublu"
 mkdir -p "${DEST_DIR}"
 
-# Enable nullglob to avoid patterns when no matches
-shopt -s nullglob
-# Scan for image files (you can add more extensions if needed)
-IMAGES=("${IMAGE_DIR}"/*.{jpg,jpeg,png})
-# Check if any images were found
-if [[ ${#IMAGES[@]} -eq 0 ]]; then
-    echo "No images found in ${IMAGE_DIR}."
-    exit 1
+# wallpaper selector
+wallpaper_Selector() {
+    WALLPAPER_DIR="${HOME}/Pictures/Wallpapers"
+
+    # Enable nullglob to avoid patterns when no matches
+    shopt -s nullglob
+    # Scan for image files (you can add more extensions if needed)
+    IMAGES=("${WALLPAPER_DIR}"/*.{jpg,jpeg,png})
+    # Check if any images were found
+    if [[ ${#IMAGES[@]} -eq 0 ]]; then
+        echo "No images found in ${IMAGE_DIR}."
+        exit 1
+    fi
+
+    # gum list selector
+    IMAGE=$(gum choose --height=30 --selected.bold --selected.underline "${IMAGES[@]}")
+
+    # overwrite wallpaper
+    cp --force "${IMAGE}" "${DEST_DIR}/wallpaper"
+    echo "Copied ${IMAGE} to ${DEST_DIR}/wallpaper."
+
+    # restart wallpaper app
+    killall -e hyprpaper && hyprpaper & disown
+}
+
+# profile selector
+profile_Selector() {
+    PROFILE_DIR="${HOME}/Pictures/Profiles"
+
+    # Enable nullglob to avoid patterns when no matches
+    shopt -s nullglob
+    # Scan for image files (you can add more extensions if needed)
+    IMAGES=("${PROFILE_DIR}"/*.{jpg,jpeg,png})
+    # Check if any images were found
+    if [[ ${#IMAGES[@]} -eq 0 ]]; then
+        echo "No images found in ${IMAGE_DIR}."
+        exit 1
+    fi
+
+    # gum list selector
+    IMAGE=$(gum choose --height=30 --selected.bold --selected.underline "${IMAGES[@]}")
+
+    # overwrite wallpaper
+    cp --force "${IMAGE}" "${DEST_DIR}/profile"
+    echo "Copied ${IMAGE} to ${DEST_DIR}/profile"
+}
+
+SELECTION=$(gum choose --height=30 --selected.bold --selected.underline "profile" "wallpaper")
+
+if [[ "$SELECTION" == "profile" ]]; then
+    echo "You selected Profile."
+    profile_Selector
+elif [[ "$SELECTION" == "wallpaper" ]]; then
+    echo "You selected Wallpaper."
+    wallpaper_Selector
+else
+    echo "No valid selection made."
 fi
-
-# gum list selector
-IMAGE=$(gum choose --height=30 --selected.bold --selected.underline "${IMAGES[@]}")
-
-# overwrite wallpaper
-cp --force "${IMAGE}" "${DEST_DIR}/wallpaper"
-echo "Copied ${IMAGE} to ${DEST_DIR}/wallpaper."
-
-# restart wallpaper app
-killall -e hyprpaper && hyprpaper & disown
